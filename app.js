ea237fa0092ccb992e569a5b14e82960ff0180b7
keylaambar <ambarkeyla3@gmail.com>
2025-08-08 12:21:13 +0700

    Update app.js
    update code app.js

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
-Vue.component("navbar-component", {
-  template: `
-    <header class="navbar">
-      <div class="left-section">
-        <button class="get-btn">Get Synapse</button>
-      </div>
+const { createApp } = VTTCue;
 
-      <nav class="middle-section">
-        <ul>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">App</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Home</a></li>
-        </ul>
-      </nav>
-
-      <div class="right-section logo">
-        <img src="images/logo.png" alt="Logo">
-      </div>
-    </header>
-  `,
-});
-
-Vue.component("slope-banner", {
-  template: `
-    <section class="slope-banner">
-      <!-- Konten opsional di dalam banner -->
-    </section>
-  `,
-});
-
-new Vue({
-  el: "#app",
-});
+createApp({
+  data() {
+    return {};
+  },
+}).mount("#app");
